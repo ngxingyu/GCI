2aaa9c10ccb978237a6eaa6932b12dc4a5e83afb
ngxingyu <ng.xingyu@dhs.sg>
2015-12-25 07:52:02 +0800

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,48 @@
+function switchA() {
+    var imageSwitch1 = document.getElementById('switcha');
+    var imageSwitchBulb = document.getElementById('switchd');
+    var imageBulb1 = document.getElementById('bulb1');
+    var imageBulb2 = document.getElementById('bulb2');
+    
+if (imageSwitchBulb.src.match("3c3ce986-aada-11e5-9f05-46f990f2c5ee")){
+imageSwitch1.src.match("3c22acc4-aada-11e5-9eec-094b4920f2dc")? imageSwitch1.src = "https://cloud.githubusercontent.com/assets/12682160/11998430/3c3d13a2-aada-11e5-89f5-83c098d851e8.png": imageSwitch1.src = "https://cloud.githubusercontent.com/assets/12682160/11998428/3c22acc4-aada-11e5-9eec-094b4920f2dc.png";
+imageBulb1.src.match("3c0436f4-aada-11e5-9a50-8d693f19684a")? imageBulb1.src = "https://cloud.githubusercontent.com/assets/12682160/11998426/3bf81b76-aada-11e5-9fd6-99d79533d9ab.png": imageBulb1.src = "https://cloud.githubusercontent.com/assets/12682160/11998427/3c0436f4-aada-11e5-9a50-8d693f19684a.png";
+
+}else{
+imageSwitch1.src.match("3c22acc4-aada-11e5-9eec-094b4920f2dc")? imageSwitch1.src = "https://cloud.githubusercontent.com/assets/12682160/11998430/3c3d13a2-aada-11e5-89f5-83c098d851e8.png": imageSwitch1.src = "https://cloud.githubusercontent.com/assets/12682160/11998428/3c22acc4-aada-11e5-9eec-094b4920f2dc.png";
+imageBulb2.src.match("3c0436f4-aada-11e5-9a50-8d693f19684a")? imageBulb2.src = "https://cloud.githubusercontent.com/assets/12682160/11998426/3bf81b76-aada-11e5-9fd6-99d79533d9ab.png": imageBulb2.src = "https://cloud.githubusercontent.com/assets/12682160/11998427/3c0436f4-aada-11e5-9a50-8d693f19684a.png";}
+}
+
+function switchB() {
+    var imageSwitch2 = document.getElementById('switchb');
+    var imageSwitchBulb = document.getElementById('switchd');
+    var imageBulb1 = document.getElementById('bulb1');
+    var imageBulb2 = document.getElementById('bulb2');
+    
+if (imageSwitchBulb.src.match("3c3ce986-aada-11e5-9f05-46f990f2c5ee")){
+imageSwitch2.src.match("3c22acc4-aada-11e5-9eec-094b4920f2dc")? imageSwitch2.src = "https://cloud.githubusercontent.com/assets/12682160/11998430/3c3d13a2-aada-11e5-89f5-83c098d851e8.png": imageSwitch2.src = "https://cloud.githubusercontent.com/assets/12682160/11998428/3c22acc4-aada-11e5-9eec-094b4920f2dc.png";
+imageBulb1.src.match("3c0436f4-aada-11e5-9a50-8d693f19684a")? imageBulb1.src = "https://cloud.githubusercontent.com/assets/12682160/11998426/3bf81b76-aada-11e5-9fd6-99d79533d9ab.png": imageBulb1.src = "https://cloud.githubusercontent.com/assets/12682160/11998427/3c0436f4-aada-11e5-9a50-8d693f19684a.png";
+
+}else{
+imageSwitch2.src.match("3c22acc4-aada-11e5-9eec-094b4920f2dc")? imageSwitch2.src = "https://cloud.githubusercontent.com/assets/12682160/11998430/3c3d13a2-aada-11e5-89f5-83c098d851e8.png": imageSwitch2.src = "https://cloud.githubusercontent.com/assets/12682160/11998428/3c22acc4-aada-11e5-9eec-094b4920f2dc.png";
+imageBulb2.src.match("3c0436f4-aada-11e5-9a50-8d693f19684a")? imageBulb2.src = "https://cloud.githubusercontent.com/assets/12682160/11998426/3bf81b76-aada-11e5-9fd6-99d79533d9ab.png": imageBulb2.src = "https://cloud.githubusercontent.com/assets/12682160/11998427/3c0436f4-aada-11e5-9a50-8d693f19684a.png";}
+}
+
+function switchC() {
+    var imageSwitch3 = document.getElementById('switchc');
+    var imageSwitchBulb = document.getElementById('switchd');
+    var imageBulb1 = document.getElementById('bulb1');
+    var imageBulb2 = document.getElementById('bulb2');
+    
+if (imageSwitchBulb.src.match("3c3ce986-aada-11e5-9f05-46f990f2c5ee")){
+imageSwitch3.src.match("3c22acc4-aada-11e5-9eec-094b4920f2dc")? imageSwitch3.src = "https://cloud.githubusercontent.com/assets/12682160/11998430/3c3d13a2-aada-11e5-89f5-83c098d851e8.png": imageSwitch3.src = "https://cloud.githubusercontent.com/assets/12682160/11998428/3c22acc4-aada-11e5-9eec-094b4920f2dc.png";
+imageBulb1.src.match("3c0436f4-aada-11e5-9a50-8d693f19684a")? imageBulb1.src = "https://cloud.githubusercontent.com/assets/12682160/11998426/3bf81b76-aada-11e5-9fd6-99d79533d9ab.png": imageBulb1.src = "https://cloud.githubusercontent.com/assets/12682160/11998427/3c0436f4-aada-11e5-9a50-8d693f19684a.png";
+
+}else{
+imageSwitch3.src.match("3c22acc4-aada-11e5-9eec-094b4920f2dc")? imageSwitch3.src = "https://cloud.githubusercontent.com/assets/12682160/11998430/3c3d13a2-aada-11e5-89f5-83c098d851e8.png": imageSwitch3.src = "https://cloud.githubusercontent.com/assets/12682160/11998428/3c22acc4-aada-11e5-9eec-094b4920f2dc.png";
+imageBulb2.src.match("3c0436f4-aada-11e5-9a50-8d693f19684a")? imageBulb2.src = "https://cloud.githubusercontent.com/assets/12682160/11998426/3bf81b76-aada-11e5-9fd6-99d79533d9ab.png": imageBulb2.src = "https://cloud.githubusercontent.com/assets/12682160/11998427/3c0436f4-aada-11e5-9a50-8d693f19684a.png";}
+}
+function switchD(){
+    var imageSwitch4 = document.getElementById('switchd');
+    imageSwitch4.src.match("3c3ce986-aada-11e5-9f05-46f990f2c5ee")? imageSwitch4.src = "https://cloud.githubusercontent.com/assets/12682160/11998424/3bf5ee46-aada-11e5-8b8e-b30258254e71.png": imageSwitch4.src = "https://cloud.githubusercontent.com/assets/12682160/11998429/3c3ce986-aada-11e5-9f05-46f990f2c5ee.png";
+}
